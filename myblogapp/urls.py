61840c16fe21cbb6a0032a777203f95cf407a040
Shunsuke Hioki <user@Shuns-MacBook-air.local>
2019-04-29 12:16:24 +0900

    add navigation bar

--- myblogapp/urls.py
+++ myblogapp/urls.py
@@ -22,7 +22,7 @@ from posts import views
 urlpatterns = [
     url(r'^posts/',include('posts.urls')),
     #url(r'^',include('posts.urls')),
-    url(r'^posts/(?P<post_id>[0-9]+)/$',views.post_detail),
+    url(r'^posts/(?P<post_id>[0-9]+)/$',views.post_detail,name='post_detail'),
     url(r'^admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
